d732b8a80dde886c5a45bc9313b5da7005dabd87
송현주 <shyunju7@naver.com>
2023-04-27 12:37:29 +0900

    ✨ feat : Profile UI 구현 및 위젯 분리

--- frontend/poom/lib/screens/profile_screen.dart
+++ frontend/poom/lib/screens/profile_screen.dart
@@ -1,12 +1,86 @@
 import 'package:flutter/material.dart';
+import 'package:poom/screens/settings_screen.dart';
+import 'package:poom/widgets/profile/profile_form.dart';
+import 'package:poom/widgets/profile/profile_menu.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
+
+  static const _textColor = Color(0xFF333333);
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  // API호출을 통해 받아올 Future 데이터 처리(userId, userEmail, profileImg)
+  // 임시 데이터 처리
+  String nickname = "songo427";
+  String email = "songo427@gmail.com";
+  String profileImgUrl = "https://avatars.githubusercontent.com/u/38373150?v=4";
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("프로필 페이지")),
+    onTapSettingButton() async {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const SettingsScreen(),
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        foregroundColor: ProfileScreen._textColor,
+        shadowColor: const Color(0xFFE4E4E4),
+        centerTitle: true,
+        elevation: 1,
+        actions: [
+          IconButton(
+            onPressed: onTapSettingButton,
+            icon: const Icon(Icons.settings),
+          ),
+        ],
+        title: const Text("나의 프로필"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 286,
+              child: ProfileForm(
+                  nickname: nickname,
+                  email: email,
+                  profileImgUrl: profileImgUrl),
+            ),
+            const Column(
+              children: [
+                SizedBox(
+                  height: 4,
+                ),
+                SizedBox(
+                  height: 60,
+                ),
+                Column(
+                  children: [
+                    MenuItem(icon: Icons.receipt, title: "나의 후원 내역"),
+                    MenuItem(
+                      icon: Icons.night_shelter_rounded,
+                      title: "보호소 회원 인증",
+                      isShelter: true,
+                    ),
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
