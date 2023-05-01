c51e79503c4b1d1759b0fbdb223327af4b43e18e
두소원 <sowondoo@naver.com>
2023-05-01 10:36:08 +0900

    :sparkles: feat: 후원 목록 가져오기 구현

--- contract/contracts/Donation.sol
+++ contract/contracts/Donation.sol
@@ -3,32 +3,73 @@ pragma solidity >= 0.8.0 <0.9.0;
 
 import "./Fundraiser.sol";
 
-contract DonationContract is Fundraiser {
+contract DonationContract is FundraiserContract {
 
     struct Donation { // 후원
         string memberId;
         uint256 donationAmount; // 후원 금액
         uint64 fundraiserId; // 모금 id
         uint256 donationTime; // 모금 시간
-        bool isIssued; // nft 발급 여부
+        uint16 isIssued; // nft 발급 여부
     }
 
 
-    mapping(uint64 => uint256) public myFundraiserCount; // shelterId => my fundraiserCount
-    mapping(uint64 => mapping(uint256=>Donation)) public donations; // 모금 id -> 후원자들
+    uint64 private _donationId;
+    mapping(string => uint64[]) public memberDonations; // memberid -> 후원id[]
+    // mapping(uint64 => uint256) public myFundraiserCount; // shelterId => my fundraiserCount
+    mapping(uint64=>Donation) public donations; //  후원 id -> 후원 내역
+    mapping(uint64 => mapping(uint64=>Donation)) public fundraiserDonationList; // 모금 id -> 후원자들
     mapping(uint64 => uint256) public donationsCount; // 모금 id -> 후원자 수
 
-        // 후원자 목록
+    /*
+     후원자 가져오려면
+     멤버별 따로 빼기
+    */
+
+
+    // 후원자 목록
     function _getDonationList(uint64 _fundraiserId) internal view returns (Donation[] memory) {
         uint256 donationLength = donationsCount[_fundraiserId];
         Donation[] memory donationList = new Donation[](donationLength);
 
-        for (uint256 i = 0; i < donationLength; i++) {
-            donationList[i] = donations[_fundraiserId][i];
+        for (uint64 i = 0; i < donationLength; i++) {
+            donationList[i] = fundraiserDonationList[_fundraiserId][i];
         }
 
         return donationList;
     }
+
+
+    // 내 후원 목록 가져오기
+    function _getMyDonationList(string memory _memberId, uint64 page, uint64 size) internal view returns(Donation[] memory){
+
+        uint256 myDonationCount = memberDonations[_memberId].length;
+
+        uint64 startIdx = page * size;
+        uint64 endIdx = startIdx + size;
+        uint256 length = endIdx > myDonationCount ? myDonationCount : endIdx;
+
+        Donation[] memory myDonaionList = new Donation[](myDonationCount);
+
+        for(uint64 i = startIdx; i < length; i++){
+            myDonaionList[i-startIdx] = donations[memberDonations[_memberId][i]];
+        }
+        return myDonaionList;
+
+    }
+
+    // nft isIssued -> 모금이 종료되면 1로 바꾸기
+    function _updtateNftIsIssued(uint64 _fundraiserId) internal {
+        uint256 donationLength = donationsCount[_fundraiserId];
+        for (uint64 i = 0; i < donationLength; i++) {
+            fundraiserDonationList[_fundraiserId][i].isIssued = 1;
+        }
+    }
+
+    // 후원
+    // function _donate()
+
+
 
 
 }
--- contract/contracts/Fundraiser.sol
+++ contract/contracts/Fundraiser.sol
@@ -6,31 +6,33 @@ contract FundraiserContract {
 
     struct Fundraiser { // 모금
         string shelterId;
+        address shelterWalet;
         string hashString; // ipfs hash
         uint256 currentAmount; // 현재 모인 금액
         bool isEnded; // 종료 되었는지, defautl false
     }
 
-    uint64 private _fundraiserIdx; 
+    uint64 private _fundraiserIdx;
     mapping(uint64 => Fundraiser) public fundraisers; // 모금
 
     /*
         모금
         모금id -> 모금
         모금 개수 mapping
-        나의 모금만 가져오려면? 
+        나의 모금만 가져오려면?
 
         후원
         모금 id -> (후원자 id->후원) mapping
         후원 개수 mapping
     */
-    
+
 
     /*
     1. 모금
         - 모금 등록 o
         - 모금 종료 o
-        - 모금 목록 
+            - 종료 후 입금
+        - 모금 목록
             - 전체 o
             - 보호소별 o
         - 모금 상세 o
