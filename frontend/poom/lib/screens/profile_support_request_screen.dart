72d71e9eb70ca460a256e6fd7609803974343d7d
송현주 <shyunju7@naver.com>
2023-05-01 13:36:37 +0900

    💄 style : NFT 이미지 미리보기 추가

--- frontend/poom/lib/screens/profile_support_request_screen.dart
+++ frontend/poom/lib/screens/profile_support_request_screen.dart
@@ -64,120 +64,152 @@ class SupportRequestScreen extends StatelessWidget {
                           ),
                         ),
                       ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 24),
-                        child: Row(
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(10.0),
-                              child: Image.network(
-                                "https://avatars.githubusercontent.com/u/38373150?v=4",
-                                width: 100,
-                              ),
-                            ),
-                            const SizedBox(
-                              width: 14,
-                            ),
-                            const Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Row(
-                                  children: [
-                                    Text(
-                                      "몽이",
-                                      style: TextStyle(
-                                        color: _textColor,
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.w600,
-                                      ),
-                                    ),
-                                    Icon(
-                                      Icons.ac_unit,
-                                      size: 16,
-                                      color: _textColor,
-                                    ),
-                                  ],
-                                ),
-                                SizedBox(
-                                  height: 10,
-                                ),
-                                Row(
-                                  children: [
-                                    Text(
-                                      "후원 마감일",
-                                      style: TextStyle(
-                                        fontSize: 14,
-                                        color: Color(0xFF666666),
-                                        fontWeight: FontWeight.w400,
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      width: 8,
-                                    ),
-                                    Text(
-                                      "2023.03.1",
-                                      style: TextStyle(
-                                        color: _textColor,
-                                        fontWeight: FontWeight.w400,
-                                        fontSize: 14,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                Row(
-                                  children: [
-                                    Text(
-                                      "현재 모금액",
-                                      style: TextStyle(
-                                        fontSize: 14,
-                                        color: Color(0xFF666666),
-                                        fontWeight: FontWeight.w400,
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      width: 8,
-                                    ),
-                                    Text(
-                                      "1.2345 eth",
-                                      style: TextStyle(
-                                        color: _textColor,
-                                        fontWeight: FontWeight.w400,
-                                        fontSize: 14,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                Row(
-                                  children: [
-                                    Text(
-                                      "목표 후원금",
-                                      style: TextStyle(
-                                        fontSize: 14,
-                                        color: Color(0xFF666666),
-                                        fontWeight: FontWeight.w400,
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      width: 8,
-                                    ),
-                                    Text(
-                                      "4.21 eth",
-                                      style: TextStyle(
-                                        color: _textColor,
-                                        fontWeight: FontWeight.w400,
-                                        fontSize: 14,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
+                      child: const RequestItem(),
                     );
                   },
                   itemCount: 5),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RequestItem extends StatelessWidget {
+  static const Color _textColor = Color(0xFF333333);
+  const RequestItem({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        print("아이템 클릭");
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 24),
+        child: Row(
+          children: [
+            Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10.0),
+                  child: Image.network(
+                    "https://avatars.githubusercontent.com/u/38373150?v=4",
+                    width: 100,
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10.0),
+                    child: Image.network(
+                      "https://blog.kakaocdn.net/dn/3QnFw/btrzDuGrysQ/eFkkwdOTgJnPkO9XPTIZM1/img.png",
+                      width: 32,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              width: 14,
+            ),
+            const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      "몽이",
+                      style: TextStyle(
+                        color: _textColor,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Icon(
+                      Icons.ac_unit,
+                      size: 16,
+                      color: _textColor,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "후원 마감일",
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Color(0xFF666666),
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 8,
+                    ),
+                    Text(
+                      "2023.03.1",
+                      style: TextStyle(
+                        color: _textColor,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "현재 모금액",
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Color(0xFF666666),
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 8,
+                    ),
+                    Text(
+                      "1.2345 eth",
+                      style: TextStyle(
+                        color: _textColor,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "목표 후원금",
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Color(0xFF666666),
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 8,
+                    ),
+                    Text(
+                      "4.21 eth",
+                      style: TextStyle(
+                        color: _textColor,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ],
         ),
