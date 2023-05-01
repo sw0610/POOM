26192fee3a9d9ad0bceb2acef03272dcdf5f5130
송현주 <shyunju7@naver.com>
2023-05-01 12:29:24 +0900

    ✨ feat : 나의 후원 내역 UI 및 state별 처리

--- frontend/poom/lib/screens/profile_support_screen.dart
+++ frontend/poom/lib/screens/profile_support_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:poom/widgets/profile/profile_support_item.dart';
 
 class ProfileSupportScreen extends StatefulWidget {
   const ProfileSupportScreen({super.key});
@@ -9,7 +10,7 @@ class ProfileSupportScreen extends StatefulWidget {
 
 class _ProfileSupportScreenState extends State<ProfileSupportScreen> {
   static const _textColor = Color(0xFF333333);
-  static const _primaryColor = Color(0xFFFF8E01);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,66 +51,23 @@ class _ProfileSupportScreenState extends State<ProfileSupportScreen> {
             const SizedBox(
               height: 32,
             ),
-            Container(
-              height: 92,
-              decoration: const BoxDecoration(),
-              child: Padding(
-                padding: const EdgeInsets.all(12),
-                child: Row(
-                  children: [
-                    Image.network(
-                      "https://avatars.githubusercontent.com/u/38373150?v=4",
-                      width: 68,
-                    ),
-                    const SizedBox(
-                      width: 12,
-                    ),
-                    const Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "귀여운 몽이",
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                        Text("3.000 eth",
-                            style: TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w500,
-                            )),
-                        Text("2023.03.01",
-                            style: TextStyle(
-                              fontSize: 12,
-                              fontWeight: FontWeight.w300,
-                            ))
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        const Expanded(
-                          flex: 1,
-                          child: Text("상태"),
-                        ),
-                        Expanded(
-                          flex: 1,
-                          child: TextButton(
-                            style: TextButton.styleFrom(
-                              backgroundColor: _primaryColor,
-                              foregroundColor: Colors.white,
-                            ),
-                            onPressed: () {},
-                            child: const Text("발급"),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
+            Expanded(
+              child: ListView.separated(
+                itemCount: 7,
+                scrollDirection: Axis.vertical,
+                itemBuilder: (context, index) {
+                  return const SupportItem(
+                    dogName: "몽이",
+                    donateDate: "2023.05.01",
+                    donateAmount: "31.333",
+                    isIssued: 2,
+                  );
+                },
+                separatorBuilder: (context, index) => const SizedBox(
+                  height: 20,
                 ),
               ),
-            ),
+            )
           ],
         ),
       ),
