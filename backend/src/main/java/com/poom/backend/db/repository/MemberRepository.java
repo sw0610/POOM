148ff77aa60c6d8feed321102aa953710dc7df98
이재욱 <jaewook_lee@outlook.com>
2023-05-02 14:12:19 +0900

    :sparkles: feat: 멤버 서비스 인터페이스 구현

--- backend/src/main/java/com/poom/backend/db/repository/MemberRepository.java
+++ backend/src/main/java/com/poom/backend/db/repository/MemberRepository.java
@@ -3,6 +3,8 @@ package com.poom.backend.db.repository;
 import com.poom.backend.db.entity.Member;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends MongoRepository<Member, String> {
-    Member findMemberByEmail(String email);
+    Optional<Member> findMemberByEmail(String email);
 }
