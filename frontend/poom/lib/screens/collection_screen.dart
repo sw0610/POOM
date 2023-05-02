50e3b1366b1bd05d1fed73c11ff202632200eb01
송현주 <shyunju7@naver.com>
2023-05-02 12:54:09 +0900

    ✨ feat : NFT 컬렉션 페이지 gridview 적용

--- frontend/poom/lib/screens/collection_screen.dart
+++ frontend/poom/lib/screens/collection_screen.dart
@@ -1,12 +1,111 @@
 import 'package:flutter/material.dart';
 
-class CollectionScreen extends StatelessWidget {
+class CollectionScreen extends StatefulWidget {
   const CollectionScreen({super.key});
+
+  static const Color _textColor = Color(0xFF333333);
+
+  @override
+  State<CollectionScreen> createState() => _CollectionScreenState();
+}
+
+class _CollectionScreenState extends State<CollectionScreen> {
+  bool _isGrid = false;
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("컬렉션 페이지")),
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(60),
+        child: AppBar(
+          backgroundColor: Colors.white,
+          foregroundColor: CollectionScreen._textColor,
+          shadowColor: const Color(0xFFE4E4E4),
+          centerTitle: true,
+          elevation: 1,
+          title: const Text(
+            "NFT 컬렉션",
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 20,
+          horizontal: 24,
+        ),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Row(
+                  children: [
+                    Text(
+                      "나는야 강형욱",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: CollectionScreen._textColor,
+                      ),
+                    ),
+                    Text(
+                      "님의 품",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        color: CollectionScreen._textColor,
+                      ),
+                    ),
+                  ],
+                ),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _isGrid = !_isGrid;
+                    });
+                  },
+                  child: Icon(
+                      _isGrid ? Icons.view_agenda : Icons.grid_view_rounded),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: _isGrid ? 2 : 1,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 5,
+                ),
+                itemCount: 10,
+                itemBuilder: (BuildContext context, int index) {
+                  return SizedBox(
+                    width: 146,
+                    height: 146,
+                    child: Card(
+                      clipBehavior: Clip.hardEdge,
+                      shape: RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.circular(20.0), // 원하는 모서리 반지름 값 설정
+                      ),
+                      child: Image.network(
+                        "https://static.vecteezy.com/system/resources/previews/004/814/513/original/cute-dog-illustration-suitable-for-pet-shop-logo-vector.jpg",
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
