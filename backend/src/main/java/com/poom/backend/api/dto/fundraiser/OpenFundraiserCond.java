e818bf0b04341f082a1ee21c715307318c4625d8
이재욱 <jaewook_lee@outlook.com>
2023-05-03 10:56:21 +0900

    :sparkle: feat: 후원 모집 등록 요청에 사용할 DTO 정의

--- backend/src/main/java/com/poom/backend/api/dto/fundraiser/OpenFundraiserCond.java
+++ backend/src/main/java/com/poom/backend/api/dto/fundraiser/OpenFundraiserCond.java
@@ -1,4 +1,32 @@
 package com.poom.backend.api.dto.fundraiser;
 
+import com.poom.backend.enums.DogGender;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
 public class OpenFundraiserCond {
+//    {
+//        "dogName" : String,
+//            "endDate" : DateTime,
+//            "dogGender" : String,
+//            "dogAge" : int,
+//        "ageIsEstimated": Boolean
+//            - True: 추정
+//        "targetAmount" : int,
+//        "dogFeature" : Text,
+//            "walletAddress" : String,
+//    }
+    private String shelterId;
+    private String shelterEthWalletAddress;
+    private String dogName;
+    private LocalDate endDate;
+    private DogGender dogGender;
+    private int dogAge;
+    private boolean ageIsEstimated;
+    private Double targetAmount;
+    private String dogFeature;
 }
