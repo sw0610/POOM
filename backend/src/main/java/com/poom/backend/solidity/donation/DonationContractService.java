1efb93b2357e5c19fb348b2439095da4fb948cbf
두소원 <sowondoo@naver.com>
2023-05-05 20:43:58 +0900

    :sparkles: feat: Donation Contract Service 구현

---
+++ backend/src/main/java/com/poom/backend/solidity/donation/DonationContractService.java
@@ -0,0 +1,17 @@
+package com.poom.backend.solidity.donation;
+
+import com.poom.backend.api.dto.donation.SmartContractDonationDto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface DonationContractService {
+
+
+    List<SmartContractDonationDto> getDonationList(Long fundraiserId) throws Exception; // 한 후원에 대한 후원자 목록 조회
+    List<SmartContractDonationDto> getMyDonationList(String memberId) throws Exception; // 내 후원 목록 조회
+    Long donate(Long fundraiserId, String memberId, LocalDateTime donationTime) throws Exception; // 후원
+    
+
+
+}
---
+++ backend/src/main/java/com/poom/backend/solidity/donation/DonationContractServiceImpl.java
@@ -0,0 +1,58 @@
+package com.poom.backend.solidity.donation;
+
+import com.poom.backend.api.dto.donation.SmartContractDonationDto;
+import com.poom.backend.config.Web3jConfig;
+import com.poom.backend.util.ConvertUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.web3j.poomcontract.PoomContract;
+
+import java.math.BigInteger;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class DonationContractServiceImpl implements DonationContractService{
+
+    private PoomContract poomContract;
+    @Autowired
+    private DonationContractServiceImpl(Web3jConfig web3jConfig){
+        poomContract = web3jConfig.getContractApi();
+    }
+
+    @Override
+    public List<SmartContractDonationDto> getDonationList(Long fundraiserId) throws Exception {
+
+        List<PoomContract.Donation> donationContractList =
+                poomContract.getDontaionList(BigInteger.valueOf(fundraiserId)).send();
+
+        List<SmartContractDonationDto> donationList
+                 = donationContractList.stream()
+                .map(donation -> SmartContractDonationDto.fromDonationContract(donation))
+                .collect(Collectors.toList());
+
+        return donationList;
+    }
+
+    @Override
+    public List<SmartContractDonationDto> getMyDonationList(String memberId) throws Exception {
+        List<PoomContract.Donation> myDonationContractList =
+                poomContract.getMyDonationList(memberId).send();;
+
+        List<SmartContractDonationDto> myDonationList
+                = myDonationContractList.stream()
+                .map(donation -> SmartContractDonationDto.fromDonationContract(donation))
+                .collect(Collectors.toList());
+
+        return myDonationList;
+    }
+
+    @Override
+    public Long donate(Long fundraiserId, String memberId, LocalDateTime donationTime) throws Exception {
+
+        poomContract.donate(BigInteger.valueOf(fundraiserId), memberId, ConvertUtil.dateTimeToBigInteger(donationTime)).send();
+
+        return null;
+    }
+}
