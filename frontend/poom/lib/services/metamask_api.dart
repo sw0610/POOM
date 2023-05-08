871770994632dea276c95494efa2a6c0c7c6ebf2
송현주 <shyunju7@naver.com>
2023-05-08 10:08:44 +0900

    ✨ feat : metaMaskAPI & delete testScreen

---
+++ frontend/poom/lib/services/metamask_api.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+import 'package:walletconnect_dart/walletconnect_dart.dart';
+
+class MetamaskApi {
+  late final _session, _uri;
+
+  // walletConnect 연결
+  final connector = WalletConnect(
+    bridge: 'https://bridge.walletconnect.org',
+    clientMeta: const PeerMeta(
+      name: 'POOM',
+      description: 'An app for converting pictures to NFT',
+      url: 'https://walletconnect.org',
+      icons: [
+        'https://velog.velcdn.com/images/taebong1012/profile/64d88125-3ac8-461d-8537-40dad38b0097/image.png'
+      ],
+    ),
+  );
+
+  // metamask 연결
+  connectMetamask(BuildContext context) async {
+    if (!connector.connected) {
+      try {
+        var session = await connector.createSession(onDisplayUri: (uri) async {
+          _uri = uri;
+          await launchUrlString(uri, mode: LaunchMode.externalApplication);
+        });
+
+        // session.accounts[0]
+        // session.chainId
+
+        // session 반영
+        _session = session;
+        print("[Metamask] connected $session.accounts[0]");
+      } catch (error) {
+        print("connectMetamask Error: $error");
+      }
+    }
+  }
+}
