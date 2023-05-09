e10f4770a2f7c57c338f59a67cfb2ae953766449
이재욱 <jaewook_lee@outlook.com>
2023-05-09 17:12:29 +0900

    :sparkles: feat: refresh 토큰 일치 여부 확인 추가

---
+++ backend/src/test/java/com/poom/backend/api/service/donation/DonationServiceImplTest.java
@@ -0,0 +1,17 @@
+package com.poom.backend.api.service.donation;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class DonationServiceImplTest {
+
+    @Test
+    public void donationListTest(){
+
+    }
+
+}
