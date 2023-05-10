b98c5ab901b1735fa4625bb7ce7e906b070dc080
두소원 <sowondoo@naver.com>
2023-05-10 17:03:54 +0900

    :sparkles: feat: 후원 페이지네이션에 hasMore 추가

---
+++ backend/src/main/java/com/poom/backend/api/dto/donation/DonationDto.java
@@ -0,0 +1,26 @@
+package com.poom.backend.api.dto.donation;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class DonationDto {
+
+  private Long donationId;
+  private Long fundraiserId;
+  private String nftImgUrl;
+  private String dogName;
+  private Double donateAmount;
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yy.MM.dd", timezone = "Asia/Seoul")
+  private LocalDateTime donateDate;
+  private int isIssued;
+
+}
