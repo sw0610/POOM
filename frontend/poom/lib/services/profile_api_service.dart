3bb78af2adbd8a608808d863b3ca45989e36334d
송현주 <shyunju7@naver.com>
2023-05-11 14:14:15 +0900

    ✨ feat : 프로필 이미지 조회 로그 설정 추가

--- frontend/poom/lib/services/profile_api_service.dart
+++ frontend/poom/lib/services/profile_api_service.dart
@@ -1,21 +1,30 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 import 'package:poom/models/profile/user_info_model.dart';
 import 'package:poom/services/auth_dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ProfileApiService {
   static const baseUrl = "https://k8a805.p.ssafy.io/api";
 
   Future<UserInfoModel> getUserProfile(BuildContext context) async {
+    var logger = Logger();
     try {
       var dio = await authDio(context);
       final response = await dio.get("/members");
-      print("response? $response");
+
+      SharedPreferences preferences = await SharedPreferences.getInstance();
+      await preferences.setString('nickname', response.data['nickname']);
+      await preferences.setString(
+          'profileImgUrl', response.data['profileImgUrl']);
+
       var responseToString = response.toString();
+      logger.i("[ProfileApiService] getUserProfile() success");
       return UserInfoModel.fromJson(jsonDecode(responseToString));
     } catch (e) {
-      print("[ProfileApiService] getUserProfile() : $e");
+      logger.e("[ProfileApiService] getUserProfile() fail $e");
       throw Error();
     }
   }
