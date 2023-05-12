c43f144a9ccac2c93beb1e4882c6ff66f0e49f5a
송현주 <shyunju7@naver.com>
2023-05-12 16:15:20 +0900

    🐛 fix : shelterStatus 타입 변경

--- frontend/poom/lib/models/profile/user_info_model.dart
+++ frontend/poom/lib/models/profile/user_info_model.dart
@@ -1,7 +1,7 @@
 // 프로필 - 사용자의 프로필 정보를 담을 모델 클래스
 class UserInfoModel {
   final String memberId, nickname, profileImgUrl, email;
-  final int? shelterStatus;
+  final String? shelterStatus;
   final String? shelterId;
 
   UserInfoModel.fromJson(Map<String, dynamic> json)
