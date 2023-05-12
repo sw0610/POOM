6b264877c7679902f09b26d78118a65d23b65fc9
송현주 <shyunju7@naver.com>
2023-05-12 13:35:27 +0900

    ✨ Feat : NFT 컬렉션 페이지 처리 및 스켈레톤 UI 적용

--- frontend/poom/lib/services/nft_api.dart
+++ frontend/poom/lib/services/nft_api.dart
@@ -3,14 +3,24 @@ import 'package:logger/logger.dart';
 import 'package:poom/services/auth_dio.dart';
 
 class NftApiService {
-  void getUserNFTList(
+  Future<Map<String, dynamic>> getUserNFTList(
       BuildContext context, int pageNum, String memberId) async {
     var logger = Logger();
+
     try {
       var dio = await authDio(context);
       final response = await dio
           .get("/members/nfts?size=10&page=$pageNum&memberId=$memberId");
+
+      Map<String, dynamic> result = {
+        "hasMore": response.data["hasMore"],
+        "nickname": response.data["nickname"],
+        "nftCount": response.data["nftCount"],
+        "nftImgUrls": response.data["nftImgUrls"]
+      };
+
       logger.i("[NftApiService] getUserNFTList() success $response");
+      return result;
     } catch (e) {
       logger.e("[NftApiService] getUserNFTList() fail $e");
       throw Error();
