1331858021857a95c9ae492f2dea4a88f2e3c296
두소원 <sowondoo@naver.com>
2023-05-16 12:57:38 +0900

    :recycle: refactor: 코드 정리

--- backend/src/main/java/com/poom/backend/api/dto/fundraiser/OpenFundraiserCond.java
+++ backend/src/main/java/com/poom/backend/api/dto/fundraiser/OpenFundraiserCond.java
@@ -1,13 +1,11 @@
 package com.poom.backend.api.dto.fundraiser;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.poom.backend.enums.DogGender;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
@@ -16,17 +14,6 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class OpenFundraiserCond {
-//    {
-//        "dogName" : String,
-//            "endDate" : DateTime,
-//            "dogGender" : String,
-//            "dogAge" : int,
-//        "ageIsEstimated": Boolean
-//            - True: 추정
-//        "targetAmount" : int,
-//        "dogFeature" : Text,
-//            "walletAddress" : String,
-//    }
 
     @NotBlank(message = "지갑 주소가 비어있습니다.")
     private String shelterEthWalletAddress;
@@ -35,7 +22,7 @@ public class OpenFundraiserCond {
     @Builder.Default
     private LocalDateTime startDate = LocalDateTime.now();
     @NotNull(message = "종료 날짜가 비어있습니다.")
-    @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss.SSS")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss.SSS")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSS")
     private LocalDateTime endDate;
     @NotNull(message = "강아지 성별이 비어있습니다.")
--- backend/src/main/java/com/poom/backend/api/service/nft/NftServiceImpl.java
+++ backend/src/main/java/com/poom/backend/api/service/nft/NftServiceImpl.java
@@ -2,7 +2,6 @@ package com.poom.backend.api.service.nft;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.poom.backend.api.dto.fundraiser.FundraiserDetailRes;
-import com.poom.backend.api.dto.fundraiser.SmartContractFundraiserDto;
 import com.poom.backend.api.dto.nft.NftIssueCond;
 import com.poom.backend.api.dto.nft.NftListRes;
 import com.poom.backend.api.dto.nft.NftMetadata;
@@ -11,33 +10,26 @@ import com.poom.backend.api.service.donation.DonationService;
 import com.poom.backend.api.service.fundraiser.FundraiserService;
 import com.poom.backend.api.service.ipfs.IpfsService;
 import com.poom.backend.api.service.member.MemberService;
-import com.poom.backend.config.jwt.JwtFilter;
 import com.poom.backend.db.entity.Member;
 import com.poom.backend.db.repository.MemberRepository;
 import com.poom.backend.exception.BadRequestException;
 import com.poom.backend.solidity.donation.DonationContractService;
 import com.poom.backend.solidity.nft.NftContractService;
-import com.poom.backend.util.ByteArrayMultipartFile;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.io.IOUtils;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
-import javax.swing.text.html.Option;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
-import java.math.BigInteger;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -94,8 +86,7 @@ public class NftServiceImpl implements NFTService {
         int nftIsIssued = donationService.getNftIsIssued(nftIssueCond.getDonationId());
         if (nftIsIssued == 0) {
             throw new BadRequestException("후원 진행중");
-        }
-        else if(nftIsIssued == 1){
+        } else if (nftIsIssued == 1) {
             throw new BadRequestException("발급 완료된 NFT입니다.");
         }
         // 종료 되었고 후원 순위 해시 있는지 체크하기
@@ -159,65 +150,65 @@ public class NftServiceImpl implements NFTService {
     public String createNftImage(String imageUrl, int rank) throws IOException, FontFormatException {
         MultipartFile nftImageFile = ipfsService.downloadImage(imageUrl);// 이미지 url->multipart file
 
-            BufferedImage originalImage = ImageIO.read(nftImageFile.getInputStream());
+        BufferedImage originalImage = ImageIO.read(nftImageFile.getInputStream());
 
-            BufferedImage highQualityImage = new BufferedImage(originalImage.getWidth(), originalImage.getHeight(), BufferedImage.TYPE_INT_RGB);
-            Graphics2D graphics = highQualityImage.createGraphics();
-            graphics.drawImage(originalImage, 0, 0, null);
-            graphics.dispose();
+        BufferedImage highQualityImage = new BufferedImage(originalImage.getWidth(), originalImage.getHeight(), BufferedImage.TYPE_INT_RGB);
+        Graphics2D graphics = highQualityImage.createGraphics();
+        graphics.drawImage(originalImage, 0, 0, null);
+        graphics.dispose();
 
-            // 글자 쓰기
-            graphics = highQualityImage.createGraphics();
-            graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        // 글자 쓰기
+        graphics = highQualityImage.createGraphics();
+        graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
 
-            // 글자 색상 설정 (배경에 맞게 선택)
-            graphics.setColor(Color.WHITE); // 예시로 흰색으로 설정
+        // 글자 색상 설정 (배경에 맞게 선택)
+        graphics.setColor(Color.WHITE); // 예시로 흰색으로 설정
 
-            // 폰트 로드
-            InputStream is = getClass().getResourceAsStream("/fontB.ttf");
-            Font customFont = Font.createFont(Font.TRUETYPE_FONT, is);
-            graphics.setFont(customFont.deriveFont(Font.BOLD, 30));
+        // 폰트 로드
+        InputStream is = getClass().getResourceAsStream("/fontB.ttf");
+        Font customFont = Font.createFont(Font.TRUETYPE_FONT, is);
+        graphics.setFont(customFont.deriveFont(Font.BOLD, 30));
 
-            // 글자 위치 설정
-            int x = 10;
-            int y = 30;
+        // 글자 위치 설정
+        int x = 10;
+        int y = 30;
 
-            // 배경과 대비되는 테두리 그리기
-            graphics.setStroke(new BasicStroke(10)); // 테두리 굵기 설정
-            graphics.setColor(Color.RED); // 테두리 색상 설정
-            graphics.drawString("#" + rank, x - 1, y);
-            graphics.drawString("#" + rank, x + 1, y);
-            graphics.drawString("#" + rank, x, y - 1);
-            graphics.drawString("#" + rank, x, y + 1);
+        // 배경과 대비되는 테두리 그리기
+        graphics.setStroke(new BasicStroke(10)); // 테두리 굵기 설정
+        graphics.setColor(Color.RED); // 테두리 색상 설정
+        graphics.drawString("#" + rank, x - 1, y);
+        graphics.drawString("#" + rank, x + 1, y);
+        graphics.drawString("#" + rank, x, y - 1);
+        graphics.drawString("#" + rank, x, y + 1);
 
-            // 실제 글자 그리기
-            graphics.setColor(Color.white); // 글자 색상 설정
-            graphics.drawString("#" + rank, x, y);
+        // 실제 글자 그리기
+        graphics.setColor(Color.white); // 글자 색상 설정
+        graphics.drawString("#" + rank, x, y);
 
-            // 이미지 파일 저장
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            ImageIO.write(highQualityImage, "jpg", baos);
-            byte[] bytes = baos.toByteArray();
+        // 이미지 파일 저장
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(highQualityImage, "jpg", baos);
+        byte[] bytes = baos.toByteArray();
 
-            // 파일 이름 및 헤더 설정
-            String fileName = nftImageFile.getOriginalFilename();
-            String modifiedFileName = "signed_" + fileName;
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.IMAGE_JPEG);
-            headers.setContentDispositionFormData("attachment", modifiedFileName);
+        // 파일 이름 및 헤더 설정
+        String fileName = nftImageFile.getOriginalFilename();
+        String modifiedFileName = "signed_" + fileName;
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_JPEG);
+        headers.setContentDispositionFormData("attachment", modifiedFileName);
 
-            // MultipartFile 생성
-            ByteArrayInputStream contentStream = new ByteArrayInputStream(bytes);
-            FileItem fileItem = new DiskFileItemFactory().createItem(modifiedFileName, MediaType.IMAGE_JPEG_VALUE, true, modifiedFileName);
-            try (InputStream in = contentStream; OutputStream out = fileItem.getOutputStream()) {
-                IOUtils.copy(in, out);
-            } catch (IOException e) {
-                throw new IllegalArgumentException("Error copying file", e);
-            }
-            MultipartFile multipartFile = new CommonsMultipartFile(fileItem);
+        // MultipartFile 생성
+        ByteArrayInputStream contentStream = new ByteArrayInputStream(bytes);
+        FileItem fileItem = new DiskFileItemFactory().createItem(modifiedFileName, MediaType.IMAGE_JPEG_VALUE, true, modifiedFileName);
+        try (InputStream in = contentStream; OutputStream out = fileItem.getOutputStream()) {
+            IOUtils.copy(in, out);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Error copying file", e);
+        }
+        MultipartFile multipartFile = new CommonsMultipartFile(fileItem);
 
-            // 이미지 업로드
-            String hash = ipfsService.uploadImage(multipartFile);
+        // 이미지 업로드
+        String hash = ipfsService.uploadImage(multipartFile);
 
 
         return hash;
